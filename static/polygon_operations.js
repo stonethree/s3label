63a9a0dd7bbfafd902bdd7c5847d0485508b302c
Shaun Irwin <shaun.irwin@gmail.com>
2018-04-02 20:55:26 +0200

    Add select polygon tool
    Select polygon using mouse clicks. Selected polygon can then be deleted
    or appended to etc.

--- static/polygon_operations.js
+++ static/polygon_operations.js
@@ -8,3 +8,26 @@ function convertPathToPolygon(path) {
         inverted: false
     };
 }
+
+function isPointInPolygon(x, y, cornersX, cornersY) {
+
+    var i, j = cornersX.length - 1;
+    var oddNodes = false;
+
+    var polyX = cornersX;
+    var polyY = cornersY;
+
+    for (i = 0; i < cornersX.length; i++) {
+        if ((polyY[i] < y && polyY[j] >= y || polyY[j] < y && polyY[i] >= y) && (polyX[i] <= x || polyX[j] <= x)) {
+            oddNodes ^= (polyX[i] + (y - polyY[i]) / (polyY[j] - polyY[i]) * (polyX[j] - polyX[i]) < x);
+        }
+        j = i;
+    }
+
+    if (oddNodes == 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
