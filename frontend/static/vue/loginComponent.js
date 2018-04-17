304ad28eaff47e1f746c989ede04c491957d9f65
Shaun Irwin <shaun.irwin@gmail.com>
2018-04-17 23:07:02 +0200

    User can log in
    Added a Vue component to allow user to log in

--- frontend/static/vue/loginComponent.js
+++ frontend/static/vue/loginComponent.js
@@ -3,10 +3,48 @@ axios.defaults.baseURL = 'http://127.0.0.1:5000/image_labeler/api/v1.0/';
 Vue.component('s3label-login', {
   data: function () {
     return {
-      count: 0
+      email: null,
+      password: null
     }
   },
-  template: '<button v-on:click="count++">Login {{ count }} times.</button>'
+  computed: {
+    userIsLoggedIn: function () {
+      return !(localStorage.s3_access_token === undefined);
+    }
+  },
+  methods: {
+    logUserIn: function () {
+        // get access token from server to permit user to make requests to the backend
+
+        const vm = this;
+
+        axios.post('login', {
+            email: vm.email,
+            password: vm.password
+          })
+          .then(function (response) {
+            var access_token = response.data.access_token;
+
+            // store access token in localStorage
+            localStorage.setItem("s3_access_token", access_token);
+
+            console.log('User logged in successfully');
+          })
+          .catch(function (error) {
+            console.log('Log in unsuccessful:', error);
+          });
+    },
+  },
+  template: `<div class="container">
+      <label for="user_email"><b>Email</b></label>
+      <input type="email" placeholder="Enter email address" name="user_email" required v-model="email">
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter password" name="psw" required v-model="password">
+
+      <button type="submit" v-on:click="logUserIn">"Login"</button>
+      </form>
+    </div>`
 })
 
 Vue.component('label-task-chooser', {
