86f56330f17a766307432d1170cb51a4170c3ccd
Shaun Irwin <shaun.irwin@gmail.com>
2018-04-30 15:42:44 +0200

    wip Adding unit test for getting next unlabeled image
    Unit test currently failing: the underlying SQL query is wrong!
    Should be able to get fresh images from label task 5, but it
    isn't currently returning any. Might need to create a different SQL
    view. CHeck that the other queries are correct!

--- backend/unit_tests/test_endpoints.py
+++ backend/unit_tests/test_endpoints.py
@@ -39,14 +39,19 @@ def test_get_label_tasks(auth, refresh_db_once):
     assert label_tasks[0]['title'] == 'Rock particle segmentation'
 
 
-# def test_get_next_unlabeled_image(auth):
-#     auth.login()
-#
-#     rv_get_tasks = auth.client.get(auth.base_url + '/label_tasks', headers=auth.auth_header())
-#
-#     assert rv_get_tasks.status_code == 200
-#
-#     label_tasks = json_of_response(rv_get_tasks)
-#
-#     assert label_tasks[0]['label_task_id'] == 1
-#     assert label_tasks[0]['title'] == 'Rock particle segmentation'
+def test_get_next_unlabeled_image(auth, refresh_db_every_time):
+    auth.login()
+
+    rv_next_im = auth.client.get(auth.base_url + '/unlabeled_images/label_tasks/5?shuffle=false',
+                                 headers=auth.auth_header())
+
+    assert rv_next_im.status_code == 200
+
+    next_im = json_of_response(rv_next_im)
+
+    # TODO: make several requests to get a few new images. Check that the order and the input_data_ids are correct
+
+    # assert label_tasks[0]['label_task_id'] == 1
+    # assert label_tasks[0]['title'] == 'Rock particle segmentation'
+
+    print('test')
