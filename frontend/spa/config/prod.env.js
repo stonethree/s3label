e85ec45454e18d963227867ee9c9241803447e16
Shaun Irwin <shaun.irwin@gmail.com>
2018-05-18 16:21:17 +0200

    Change API address for production env to work with SSL

--- frontend/spa/config/prod.env.js
+++ frontend/spa/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ADDR: '"http://label.stonethree.com/image_labeler/api/v1.0"'
+  API_ADDR: '"https://label.stonethree.com:8443/image_labeler/api/v1.0"'
 }
