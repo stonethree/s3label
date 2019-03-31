11c9f091e37c921dead70c77d2fca239c3eb0232
Shaun Irwin <shaun.irwin@gmail.com>
2019-03-31 11:38:15 +0200

    Remove unused ci files

--- ci/build/backend_zip.ps1
+++
@@ -1,4 +0,0 @@
-& 7z a -t7z "backend.7z" backend/
-if ($LastExitCode -gt 0) {
-    Exit $LastExitCode
-}
--- ci/build/update_front_dependencies.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Installing npm requirements"
-& cd frontend\spa
-& npm install
-if ($LastExitCode -gt 0) {
-    Exit $LastExitCode
-}
